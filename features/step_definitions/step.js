baa04c6aa6ab949c5b86f379dbb27ed73deb7f8c
magaum <weslei.paula@fatec.sp.gov.br>
2020-06-01 16:39:08 -0300

    progressão nos testes, mensagem de erro atual: ReferenceError: Person is not defined

--- features/step_definitions/step.js
+++ features/step_definitions/step.js
@@ -1,6 +1,9 @@
 const { Given, When, Then } = require('cucumber')
 
-Given('Lucy is located {int} meters from Sean', function (int) {
+Given('Lucy is located {int} meters from Sean', function (distance) {
+  this.lucy = new Person
+  this.sean = new Person
+  this.lucy.moveTo(distance);
   // Write code here that turns the phrase above into concrete actions
   return 'pending';
 });
