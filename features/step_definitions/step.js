19ab5a72132a7d25a6091ee3ba3398ec3f17bcaa
magaum <weslei.paula@fatec.sp.gov.br>
2020-06-01 16:43:27 -0300

    primeiro teste passando (Given)

--- features/step_definitions/step.js
+++ features/step_definitions/step.js
@@ -5,8 +5,6 @@ Given('Lucy is located {int} meters from Sean', function (distance) {
   this.lucy = new Person
   this.sean = new Person
   this.lucy.moveTo(distance);
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending';
 });
 
 When('Sean shouts {string}', function (string) {
